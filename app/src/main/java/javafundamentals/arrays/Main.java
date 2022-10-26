55041dd1e1169e909504401aeacab89f1deacdec
Mathew Torres <torres.mathew@gmail.com>
2022-10-25 21:51:14 -0700

    update lab 2 arrayOfArrays

--- app/src/main/java/javafundamentals/arrays/Main.java
+++ app/src/main/java/javafundamentals/arrays/Main.java
@@ -2,6 +2,7 @@ package javafundamentals.arrays;
 
 
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main
@@ -17,6 +18,15 @@ public class Main
         int sum[] = {5,6,7,8,9,10,11,12};
         calculatingAverages(sum);
 
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        int[] lowest = arrayOfArrays(weeklyMonthTemperatures);
+        String lowestAsString = Arrays.toString(lowest);
+        System.out.println(lowestAsString);
     }
     public static int[] roll(int n)
     {
@@ -48,7 +58,7 @@ public class Main
     }
 
 
-    public static int[] calculatingAverages(int[] sum)
+    public static int calculatingAverages(int[] sum)
     {
         // calculate the average of the array
 
@@ -61,9 +71,25 @@ public class Main
         int average = total / sum.length;
         System.out.println(average);
 
-        return new int[]{average};
+        return average;
     }
 
+    public static int[] arrayOfArrays(int[][] numbers)
+    {
+        int lowestAverageNumber = calculatingAverages(numbers[0]);
+        int[] lowestArray = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++)
+        {
+            int tempAverage = calculatingAverages(numbers[i]);
+            if (tempAverage < lowestAverageNumber)
+            {
+                lowestAverageNumber = tempAverage;
+                lowestArray = numbers[i];
+            }
+        }
+        return lowestArray;
+    }
 
   
     
