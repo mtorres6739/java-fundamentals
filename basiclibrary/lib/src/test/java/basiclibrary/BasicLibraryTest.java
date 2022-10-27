c71781cbe734c2726dd2af332e6bd3e5b50f7ea1
Mathew Torres <torres.mathew@gmail.com>
2022-10-26 23:33:55 -0700

    updates lab3 with linter

--- basiclibrary/lib/src/test/java/basiclibrary/BasicLibraryTest.java
+++ basiclibrary/lib/src/test/java/basiclibrary/BasicLibraryTest.java
@@ -43,4 +43,23 @@ public class BasicLibraryTest {
 //        System.out.println(lowestString);
         assertEquals(basicLibrary.arrayOfArrays(weeklyMonthTemperatures), testAns);
     }
+
+    @Test void testForAnalyzingWeatherData () {
+        BasicLibrary basicLibrary = new BasicLibrary();
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+       String output = basicLibrary.analyzingWeatherData(weeklyMonthTemperatures);
+       assertTrue(output.contains("63"));
+       assertEquals("High: 72\n" +
+               "Low: 51\n" +
+               "Never saw temperature: 63\n" +
+               "Never saw temperature: 67\n" +
+               "Never saw temperature: 68\n" +
+               "Never saw temperature: 69\n", output);
+        System.out.println(output);
+    }
 }
